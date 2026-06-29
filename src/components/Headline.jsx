e2623d753edafc5fad2a51a4fe9d68224e4dba12
Priyanshu Mehra <pm7300779625@gmail.com>
2026-06-29 21:13:54 +0530

    changing headline

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Headline() {
+  const headlines = [
+    "Full-Stack Developer",
+    "Backend Developer",
+    "Frontend Developer",
+    "Mern Stack "
+  ];
+
+  const [index, setIndex] = useState(0);
+
+
+  const nextHeadline = () => {
+    setIndex((prev) => (prev + 1) % headlines.length);
+  };
+
+
+
   return (
-    <div className="bg-violet-600 rounded-2xl p-8 text-white hover:scale-105 ease-in-out duration-700 cursor-pointer ">
+    <div
+      onClick={nextHeadline}
+      className="bg-violet-600 rounded-2xl p-8 text-white hover:scale-105 ease-in-out duration-700 cursor-pointer ">
       <p className="text-sm font-semibold text-violet-100 mb-2">Headline</p>
       <h2 className="text-4xl font-bold">
-        Backend<br />Developer
+        {headlines[index]}
       </h2>
     </div>
   );
